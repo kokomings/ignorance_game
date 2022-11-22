05741f22b448cca0c5a0495a6b3a81a1d258a2b4
plan222 <kimhojun222@naver.com>
2022-11-22 23:53:52 +0900

    캐릭터 테스트 코드 추가

---
+++ MonsterFaction/MonsterFaction/MonsterFactionTests1/Model/Character/CharacterTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonsterFaction.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterFaction.Model.Tests
+{
+    [TestClass()]
+    public class CharacterTests
+    {
+        [TestMethod()]
+        public void GetTotalStatTest()
+        {
+            var stat1 = new Stat { Attack = 10 };
+            var stat2 = new Stat { Defense = 22 };
+            var stat3 = new Stat { Attack = 7, Defense = 7 };
+
+            var character = new Character(stat1);
+            character.AddSkill(new StatSkill { Stat = stat2 });
+            character.AddSkill(new StatSkill { Stat = stat3 });
+
+            Assert.AreEqual(
+                stat1 + stat2 + stat3,
+                character.GetTotalStat()
+                );
+        }
+    }
+}
