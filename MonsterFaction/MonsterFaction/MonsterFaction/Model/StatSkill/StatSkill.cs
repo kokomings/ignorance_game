b5f444c1856810d540e195106e8070ef242ac189
plan222 <kimhojun222@naver.com>
2022-11-24 23:28:28 +0900

    StatSkill 에 Rank 추가

--- MonsterFaction/MonsterFaction/MonsterFaction/Model/StatSkill/StatSkill.cs
+++ MonsterFaction/MonsterFaction/MonsterFaction/Model/StatSkill/StatSkill.cs
@@ -3,6 +3,19 @@ namespace MonsterFaction.Model
     public readonly struct StatSkill
     {
         public readonly string Name { get; init; }
+        public readonly Rank Rank { get; init; }
         public readonly Stat Stat { get; init; }
+
+        public StatSkill(string name, Rank rank, Stat stat)
+        {
+            Name = name;
+            Stat = stat;
+            Rank = rank;
+        }
+    }
+    public enum Rank
+    {
+        S, A, B, C, D
     }
 }
+
