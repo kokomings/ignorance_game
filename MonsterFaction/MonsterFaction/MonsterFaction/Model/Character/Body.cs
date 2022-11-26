496776d97ce782e5cea29581d676fb18b082f7bd
plan222 <kimhojun222@naver.com>
2022-11-26 14:18:54 +0900

    feat: Body 클래스 작성

---
+++ MonsterFaction/MonsterFaction/MonsterFaction/Model/Character/Body.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace MonsterFaction.Model
+{
+    public class Body
+    {
+        private int baseHP;
+        private int buffHP;
+        private int currentHP;
+        private List<Buff> buffs = new List<Buff>();
+
+        public int CurrentHP { get => currentHP; }
+        public int MaxHP { get => baseHP + buffHP; }
+        public Boolean IsDead { get => currentHP <= 0; }
+
+        private Stat buffStat;
+        public Stat BuffStat { get => buffStat; }
+
+        public Body(int maxHP)
+        {
+            this.baseHP = maxHP;
+            this.currentHP = maxHP;
+            sync();
+        }
+
+        public void SetBaseMaxHP(int newHP)
+        {
+            int gap = newHP - this.baseHP;
+
+            this.baseHP = newHP;
+
+            if (gap > 0 && !IsDead)
+                this.currentHP += gap;
+            sync();
+        }
+        public void AddBuff(int duration, Stat stat)
+        {
+            buffs.Add(new Buff(duration, stat));
+            sync();
+        }
+
+        public void TimeFlow()
+        {
+            buffs = buffs.Select(buff => new Buff(buff.Duration - 1, buff.Stat)).ToList();
+            buffs.RemoveAll(buff => buff.Duration <= 0);
+            sync();
+        }
+
+        public void Heal(int heal)
+        {
+            if (!IsDead)
+                currentHP += heal;
+        }
+
+        public void Damaged(int damage)
+        {
+            this.currentHP -= damage;
+            if (this.currentHP < 0)
+                this.currentHP = 0;
+        }
+        private void sync()
+        {
+            Stat sum = new Stat();
+            foreach (var buff in buffs)
+            {
+                sum += buff.Stat;
+            }
+            this.buffStat = sum;
+            setBuffMaxHP(sum.HP);
+        }
+        private void setBuffMaxHP(int newHP)
+        {
+            int gap = newHP - this.buffHP;
+
+            this.buffHP = newHP;
+
+            if (gap > 0 && !IsDead)
+                this.currentHP += gap;
+
+            if (this.currentHP > this.baseHP + this.buffHP)
+                this.currentHP = baseHP;
+        }
+    }
+    // 밖으로 뺄 수도 있음.
+    public readonly struct Buff
+    {
+        public readonly int Duration { get; }
+        public readonly Stat Stat { get; }
+
+        public Buff(int duration, Stat stat)
+        {
+            Duration = duration;
+            Stat = stat;
+        }
+    }
+}
---
+++ MonsterFaction/MonsterFaction/MonsterFactionTests1/Model/Character/BodyTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonsterFaction.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterFaction.Model.Tests
+{
+    [TestClass()]
+    public class BodyTests
+    {
+        [TestMethod()]
+        public void SetMaxHPTest()
+        {
+            Body body = new Body(100);
+            Assert.AreEqual(100, body.CurrentHP);
+
+            body.SetBaseMaxHP(120);
+            Assert.AreEqual(120, body.CurrentHP);
+
+            body.Damaged(100);
+            Assert.AreEqual(20, body.CurrentHP);
+
+            body.SetBaseMaxHP(100);
+            Assert.AreEqual(20, body.CurrentHP);
+
+            body.SetBaseMaxHP(180);
+            Assert.AreEqual(100, body.CurrentHP);
+        }
+
+        [TestMethod()]
+        public void GetBuffStatTest()
+        {
+            Body body = new Body(100);
+
+            body.AddBuff(1, new Stat { Attack = 10 });
+            body.AddBuff(1, new Stat { Defense = 9 });
+            body.AddBuff(1, new Stat { Attack = 2, Defense = 2 });
+
+            Assert.AreEqual(new Stat { Attack = 12, Defense = 11 }, body.BuffStat);
+        }
+
+        [TestMethod()]
+        public void AddBuffHpTest()
+        {
+            Body body = new Body(100);
+
+            body.AddBuff(1, new Stat { HP = 20 });
+            Assert.AreEqual(120, body.MaxHP);
+            Assert.AreEqual(120, body.CurrentHP);
+
+            body.Damaged(20);
+            body.AddBuff(1, new Stat { HP = 30 });
+            Assert.AreEqual(150, body.MaxHP);
+            Assert.AreEqual(130, body.CurrentHP);
+        }
+
+        [TestMethod()]
+        public void TimeFlowTest()
+        {
+            Body body = new Body(100);
+
+            body.AddBuff(1, new Stat { HP = 20 });
+            body.AddBuff(2, new Stat { Attack = 10 });
+            body.AddBuff(3, new Stat { Defense = 9 });
+            body.AddBuff(4, new Stat { Attack = 2, Defense = 2 });
+            Assert.AreEqual(new Stat { HP = 20, Attack = 12, Defense = 11 }, body.BuffStat);
+            Assert.AreEqual(120, body.MaxHP);
+
+            body.TimeFlow();
+            Assert.AreEqual(new Stat { Attack = 12, Defense = 11 }, body.BuffStat);
+            Assert.AreEqual(100, body.MaxHP);
+
+            body.TimeFlow();
+            Assert.AreEqual(new Stat { Attack = 2, Defense = 11 }, body.BuffStat);
+
+            body.TimeFlow();
+            Assert.AreEqual(new Stat { Attack = 2, Defense = 2 }, body.BuffStat);
+
+            body.TimeFlow();
+            Assert.AreEqual(new Stat(), body.BuffStat);
+        }
+
+        [TestMethod()]
+        public void DeadTest()
+        {
+            Body body = new Body(100);
+            body.Damaged(120);
+
+            Assert.AreEqual(0, body.CurrentHP);
+            Assert.IsTrue(body.IsDead);
+
+            body.Heal(100);
+            Assert.AreEqual(0, body.CurrentHP);
+            Assert.IsTrue(body.IsDead);
+
+            body.AddBuff(1, new Stat { HP = 20 });
+            body.SetBaseMaxHP(150);
+            Assert.AreEqual(0, body.CurrentHP);
+            Assert.IsTrue(body.IsDead);
+        }
+    }
+}
