96172c2f68ac6498cc0575b016d640717272561f
plan222 <kimhojun222@naver.com>
2022-12-06 23:04:52 +0900

    refactor: SampleObject -> Player

--- MonsterFaction/MonsterFaction/MonsterFaction/GameWorld/WorldObject/Player.cs
+++ MonsterFaction/MonsterFaction/MonsterFaction/GameWorld/WorldObject/Player.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 namespace MonsterFaction.GameWorld.WorldObject
 {
-    public class SampleObject : IUpdatable, IDrawable
+    public class Player : IUpdatable, IDrawable
     {
         private readonly IShape shape;
         private readonly Movement movement;
@@ -12,7 +12,7 @@ namespace MonsterFaction.GameWorld.WorldObject
         public IMovement Movement => movement;
         public IMovementForPlayer PlayerMovement => movement;
 
-        public SampleObject(IShape shape, Vector3 position, Vector2 direction)
+        public Player(IShape shape, Vector3 position, Vector2 direction)
         {
             this.shape = shape;
             this.movement = new Movement(position, direction);
