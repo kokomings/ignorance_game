189bf0e5cb8c7e78132a1484293256d8427e720b
plan222 <kimhojun222@naver.com>
2022-12-23 01:44:46 +0900

    plan-6: Event 테스트 추가

---
+++ MonsterFaction/MonsterFaction/MonsterFactionTests1/SystemEvents/EventBrokerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonsterFaction.SystemEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterFaction.SystemEvents.Tests
+{
+    [TestClass()]
+    public class EventTests
+    {
+        [TestMethod()]
+        public void PublishEventTest()
+        {
+            var subsciber = new EventSubscriber<CreateEvent>(EventType.CREATE);
+            var testEvent1 = new CreateEvent { ObjectId = 10 };
+            var testEvent2 = new CreateEvent { ObjectId = 20 };
+            EventBroker.PublishEvent(testEvent1);
+            EventBroker.PublishEvent(testEvent2);
+            Assert.IsTrue(
+                subsciber.FetchAll().SequenceEqual(new List<CreateEvent>() { testEvent1, testEvent2 })
+                );
+            Assert.AreEqual(0, subsciber.FetchAll().Count());
+        }
+
+        [TestMethod()]
+        public void MultiSubscriberTest()
+        {
+            var subsciber1 = new EventSubscriber<CreateEvent>(EventType.CREATE);
+            var testEvent1 = new CreateEvent { ObjectId = 10 };
+            EventBroker.PublishEvent(testEvent1);
+
+            var testEvent2 = new CreateEvent { ObjectId = 20 };
+            var subsciber2 = new EventSubscriber<CreateEvent>(EventType.CREATE);
+            EventBroker.PublishEvent(testEvent2);
+
+            Assert.IsTrue(
+                subsciber1.FetchAll().SequenceEqual(new List<CreateEvent>() { testEvent1, testEvent2 })
+                );
+            Assert.IsTrue(
+                subsciber2.FetchAll().SequenceEqual(new List<CreateEvent>() { testEvent2 })
+                );
+        }
+    }
+}
